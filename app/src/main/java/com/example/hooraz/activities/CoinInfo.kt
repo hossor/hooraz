b555215f52fdaf77a7c407ac78167b32be355aef
hosseinorz <hossor79@gmail.com>
2021-06-14 11:38:56 +0430

    add icon to coin info xml with glide

--- app/src/main/java/com/example/hooraz/activities/CoinInfo.kt
+++ app/src/main/java/com/example/hooraz/activities/CoinInfo.kt
@@ -1,12 +1,15 @@
 package com.example.hooraz.activities
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.model.GlideUrl
 import com.example.hooraz.Model.CoinModel
 import com.example.hooraz.R
 import com.example.hooraz.Views.CoinView
+import com.github.twocoffeesoneteam.glidetovectoryou.GlideToVectorYou
 import kotlinx.android.synthetic.main.activity_coin.*
 import org.json.JSONArray
 
@@ -22,9 +25,10 @@ class CoinInfo : AppCompatActivity(), CoinView {
         if (extraCoin != null) {
             var name =extraCoin.getString("coinName").toString()
             coinModel.CoinChanges(name , this)
-            Glide.with(this)
-                .load("https://s3.us-east-2.amazonaws.com/nomics-api/static/images/currencies/btc.svg")
-                .into(this.CoinIMG)
+            GlideToVectorYou.init()
+                .with(CoinInfo@this)
+                .load(Uri.parse("https://s3.us-east-2.amazonaws.com/nomics-api/static/images/currencies/btc.svg") ,CoinIMG)
+
         }
     }
 
